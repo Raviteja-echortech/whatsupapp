6ffad8b7ae55dee06956223a78ca585eba516da9
Ravi teja <raviteja@echortech.com>
2024-10-15 17:57:46 +0530

    gestural Handler is Been Added

--- App.jsx
+++ App.jsx
@@ -1,17 +1,83 @@
-import React from "react"
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native"
+import React, { useCallback, useEffect, useState } from "react"
+import { Dimensions, FlatList, Image, ImageBackground, Keyboard, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native"
 import { SvgXml } from "react-native-svg"
 import ICONS from "./Assets/Icons"
 
 const App = () => {
+  const [messages, setMessages] = useState([{ message: "Hi", id: 1 },]);
+  const [chat, setChat] = useState("")
+  const [popUp, setPopUp] = useState(false)
+  const [isKeyboardVisible, setKeyboardVisible] = useState(false)
+  const address = "Raviteja Illuri"
+  const handleEnterkeyBoard = (e) => {
+    const putMessage = { message: chat, id: new Date() }
+    setMessages([...messages, putMessage])
+    setChat("")
+  }
+
+  // console.log(messages, "messages")
+  useEffect(() => {
+    const keyboardDidShowListener = Keyboard.addListener('keyboardDidShow',
+      () => {
+        setKeyboardVisible(true); // or some other action
+      }
+    );
+    const keyboardDidHideListener = Keyboard.addListener('keyboardDidHide',
+      () => {
+        setKeyboardVisible(false); // or some other action
+      }
+    );
+
+    return () => {
+      keyboardDidHideListener.remove();
+      keyboardDidShowListener.remove();
+    };
+  }, []);
   return (
     <SafeAreaView style={Styles.container} >
-      <View>
-        <SvgXml xml={ICONS.LeftArrow} />
-         <View>
-          <Image source={{uri:"https://t3.ftcdn.net/jpg/05/66/68/36/360_F_566683667_BeBecGFABNQYkZhquqJxhSj7UOcCY7ZJ.jpg"}}  />
-         </View>
+      <View style={Styles.headerContainer}>
+        <View style={Styles.innerHeader}>
+          <SvgXml xml={ICONS.LeftArrow} />
+          <Image
+            source={require('./Assets/PNG/humanFace.jpg')}
+            style={{ width: 35, height: 35, borderRadius: 35 / 2 }}
+          />
+          <Text style={Styles.userName} >{address} </Text>
+        </View>
+        <View style={Styles.secondinnerContainer} >
+          <SvgXml xml={ICONS.VideoCall} style={{ marginRight: 5 }} />
+          <SvgXml xml={ICONS.TelePhone} style={{ marginLeft: 5 }} />
+          <TouchableOpacity activeOpacity={0.7} style={{ marginLeft: 5 }} onPress={() => setPopUp(!popUp)}>
+            <SvgXml xml={ICONS.DotedIcons} />
+          </TouchableOpacity>
+        </View>
       </View>
+      {popUp && <View style={Styles.popUpStyles} >
+
+      </View>}
+      <ImageBackground source={require('./Assets/PNG/humanFace.jpg')} resizeMode="cover" style={Styles.behindImage} >
+        <FlatList
+          data={messages}
+          decelerationRate={0.5}
+          style={Styles.chatContainer}
+          renderItem={({ item, index }) => {
+            return (
+              <View key={item.id}  >
+                <Text style={[Styles.chatTxt, { marginBottom: index === messages.length - 1 && "17%" }]} >
+                  {item.message}
+                </Text>
+              </View>
+            )
+          }}
+          keyExtractor={item => item.id}
+        />
+        <View style={Styles.inputContainer} >
+          <TextInput  style={Styles.messageBox} placeholder="start Message ...." value={chat} onChangeText={(e) => setChat(e)} onSubmitEditing={(e) => handleEnterkeyBoard(e)} />
+          <TouchableOpacity activeOpacity={0.7} style={Styles.microPhoneBox} >
+            <SvgXml xml={ICONS.MicroPhone} />
+          </TouchableOpacity>
+        </View>
+      </ImageBackground >
     </SafeAreaView>
   )
 }
@@ -21,6 +87,100 @@ const Styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff"
   },
-  
-  
+  headerContainer: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    width: "100%",
+    justifyContent: "space-between",
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.17,
+    shadowRadius: 3.05,
+    elevation: 4,
+    position: "absolute",
+    top: 0,
+    zIndex: 1
+  },
+  innerHeader: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  secondinnerContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  userName: {
+    marginLeft: 10,
+    fontSize: 20,
+    color: "#000"
+  },
+  chatContainer: {
+    backgroundColor: "transparent",
+    paddingTop: "12%",
+    marginBottom: "20%",
+  },
+  messageBox: {
+    backgroundColor: "#fff",
+    borderRadius: 100,
+    paddingLeft: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "85%"
+  },
+  inputContainer: {
+    position: "absolute",
+    bottom: 10,
+    width: "95%",
+    flexDirection: "row",
+    alignSelf: "center",
+    justifyContent: "space-evenly",
+    alignItems: "center"
+  },
+  microPhoneBox: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    marginLeft: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "green"
+  },
+  popUpStyles: {
+    height: 200,
+    position: "absolute",
+    right: 5,
+    top: 45,
+    zIndex: 2,
+
+    width: 150,
+    backgroundColor: "#fff",
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.17,
+    shadowRadius: 3.05,
+    elevation: 4,
+    borderRadius: 10,
+  },
+  behindImage: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  chatTxt: {
+    marginTop: 5,
+    backgroundColor: "green",
+    borderRadius: 10,
+    padding: 15,
+    marginLeft: 10,
+    color: "#fff",
+    alignSelf: "flex-start",
+    // marginBottom:20
+  }
+
 })
--- Assets/Icons.js
+++ Assets/Icons.js
@@ -1,9 +1,43 @@
 const ICONS = {
-    LeftArrow: `<?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+	LeftArrow: `<?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
 <svg width="40px" height="40px" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
 <rect width="48" height="48" fill="white" fill-opacity="0.01"/>
 <path d="M12 23.9917L36 23.9917" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M24 36L12 24L24 12" stroke="#000000" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>`,
+	VideoCall: `<?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg width="30px" height="30px" viewBox="0 0 48 48" version="1" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 48 48">
+    <path fill="#4CAF50" d="M8,12h22c2.2,0,4,1.8,4,4v16c0,2.2-1.8,4-4,4H8c-2.2,0-4-1.8-4-4V16C4,13.8,5.8,12,8,12z"/>
+    <polygon fill="#388E3C" points="44,35 34,29 34,19 44,13"/>
+</svg>`,
+	TelePhone: `<?xml version="1.0" encoding="iso-8859-1"?>
+<!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg fill="#000000" height="25px" width="25px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+	 viewBox="0 0 511.988 511.988" xml:space="preserve">
+<g>
+	<g>
+		<path d="M511.686,411.188c-0.96-14.827-8.107-28.48-19.84-37.547l-68.267-53.333c-18.773-14.72-45.12-14.613-63.68,0.213
+			l-15.253,12.053c-12.053,9.493-29.333,8.533-40.213-2.347L182.299,207.775c-10.88-10.88-11.84-28.267-2.347-40.32l12.053-15.253
+			c14.72-18.667,14.827-45.013,0.213-63.787l-53.227-68.48c-17.493-22.507-49.92-26.56-72.427-9.067
+			c-1.707,1.387-3.413,2.773-4.907,4.373L18.032,58.975c-18.667,18.773-64,95.573,137.707,297.707
+			c128.213,128.32,206.187,155.307,249.067,155.307c25.493,0,40.32-9.387,48.213-17.28l43.627-43.733
+			C507.099,440.415,512.539,426.015,511.686,411.188z M483.952,433.162c-0.747,0.96-1.6,1.92-2.453,2.667l-43.627,43.733
+			c-5.013,5.013-14.827,11.093-33.067,11.093c-31.787,0-104.427-19.307-233.92-149.12C21.552,191.882,3.312,103.882,33.179,73.908
+			l43.627-43.733c5.653-5.653,13.333-8.853,21.44-8.853c0.64,0,1.28,0,1.92,0.107c8.64,0.533,16.747,4.8,21.973,11.627l53.227,68.48
+			c8.64,11.093,8.533,26.56-0.107,37.547l-12.053,15.147c-16.213,20.587-14.507,50.027,4.053,68.587l122.133,122.453
+			c18.453,18.56,48,20.267,68.48,4.053l15.253-12.053c10.88-8.64,26.347-8.747,37.333-0.107l68.267,53.333
+			C491.952,400.842,494.299,419.935,483.952,433.162z"/>
+	</g>
+</g>
+</svg>`,
+	DotedIcons: `<?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg width="30px" height="30px" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#000000" class="bi bi-three-dots-vertical">
+  <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+</svg>`,
+	MicroPhone: `<?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+<svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M19 10V12C19 15.866 15.866 19 12 19M5 10V12C5 15.866 8.13401 19 12 19M12 19V22M8 22H16M12 15C10.3431 15 9 13.6569 9 12V5C9 3.34315 10.3431 2 12 2C13.6569 2 15 3.34315 15 5V12C15 13.6569 13.6569 15 12 15Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`
+
 }
 export default ICONS
